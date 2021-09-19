3415579a9680c110c01ce3ef34b9f9ce6567de35
Talita Azevedo <talita.azevedo360@gmail.com>
2021-09-18 21:21:20 -0300

    Styled Display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import ButtonPanel from "./components/ButtonPanel";
+import Display from "./components/Display";
 export default class App extends React.Component {
     state = {
         total: null,
@@ -14,6 +15,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div className="component-app">
+                <Display value={this.state.next || this.state.total} />
                 <ButtonPanel clickHandler={this.handleClick} />
             </div>
         );
